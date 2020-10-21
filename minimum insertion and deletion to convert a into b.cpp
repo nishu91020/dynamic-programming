c3c89a2b87893db086029dff28a3b47feb623c2e
nishu91020 <rainishu111@gmail.com>
2020-10-22 00:44:51 +0530

    min insertion & deletion

---
+++ minimum insertion and deletion to convert a into b.cpp
@@ -0,0 +1,33 @@
+//problem statement:we are given two strings a and b we need to fin minimum number of insertion and deletion operation required to convert string a into b
+#include<bits/stdc++.h>
+using namespace std;
+
+int LCS(string a, string b, int m,int n)
+{
+	int t[m-1][n-1];
+	for(int i=0;i<m+1;i++)
+	{
+		for(int j=0;j<n+1;j++)
+		{
+			if(i==0 || j==0)
+				t[i][j]=0;
+			else if(a[i-1]==b[j-1])
+				t[i][j]=1+t[i-1][j-1];
+			else
+				t[i][j]=max(t[i-1][j],t[i][j-1]);
+		}
+	}
+	return t[m][n];
+}
+int main()
+{
+	string a,b;
+	cout<<"Enter string a and b"<<"\n";
+	cin>>a>>b;
+	int m=a.length();
+	int n=b.length();
+	int x=LCS(a,b,a.length(),b.length());
+	cout<<"minimum number of deletions = "<<m-x<<"\n";
+	cout<<"minimum number of insertions = "<<n-x<<"\n";
+	return 0;
+}
