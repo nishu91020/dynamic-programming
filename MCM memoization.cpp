3a4278aa87ad89bd3e3ed9ceef18143b05f697ed
nishu91020 <rainishu111@gmail.com>
2020-10-24 17:10:12 +0530

    MCM memoized

---
+++ MCM memoization.cpp
@@ -0,0 +1,34 @@
+//problem statement:matrix chain multiplication using memoized version.
+#include<bits/stdc++.h>
+
+using namespace std;
+	int n;
+int MCM(int a[],int i,int j)
+{
+	int t[n+1][n+1];
+	memset(t,-1,sizeof(t));
+	if(i>=j)
+		return 0;
+	if(t[i][j]!=-1)
+		return t[i][j];
+	int ans=INT_MAX;
+	for(int k=i;k<j;k++)
+	{
+		int x=t[i][k]+t[k+1][j]+(a[i-1]*a[k]*a[j]);
+		ans=min(x,ans);
+	}
+	return t[i][j]=ans;
+}
+int main()
+{
+	cout<<"enter no. of elements you want to enter in array"<<"\n";
+	cin>>n;	
+	
+	int arr[n];
+	cout<<"enter array"<<"\n";
+	for(int i=0;i<n;i++)
+		cin>>arr[i];
+	int x=MCM(arr,1,n-1);
+	cout<<"minimum cost of multiplication = "<<x<<"\n";
+	return 0;
+}
